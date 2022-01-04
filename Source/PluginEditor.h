9def7d6f5c6804bf16c8e623de2d51489dd8e8f1
nablum <hugo.besnard.pro@gmail.com>
2022-01-04 15:00:53 +0100

    Added Sliders to GUI

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,17 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+//Custor rotary slider
+struct customRotarySlider : juce::Slider
+{
+    customRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                        juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+        
+    }
+};
+
+
 //==============================================================================
 /**
 */
@@ -28,6 +39,17 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     ZooEQAudioProcessor& audioProcessor;
+    
+    customRotarySlider peakFreqSlider,
+    peakGainSlider,
+    peakQualitySlider,
+    lowCutFreqSlider,
+    highCutFreqSlider,
+    lowCutSlopeSlider,
+    highCutSlopeSlider;
+    
+    std::vector<juce::Component*> getComps();
+    
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ZooEQAudioProcessorEditor)
 };
